c46522e54aa3ee31657e1cf925b038fe3330920f
dudaodong <dudaodong@gmail.com>
2022-10-23 18:29:49 +0800

    add basic type int and float demo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    variable_declare();
-    variable_destruct();
+    // variable_declare();
+    // variable_destruct();
+    // variable_shadow();
 
-    variable_shadow();
+    variable_int_float();
 }
 
 // 变量绑定、可变性
@@ -45,10 +46,46 @@ fn variable_shadow() {
 
     let x = x + 1;
     {
-        let x = x*2;
+        let x = x * 2;
         println!("innter x is: {}", x)
     }
 
     println!("outter x is: {}", x)
+}
 
+/**
+ * rust基本数据类型
+ * 数值类型: 有符号整数 (i8, i16, i32, i64, isize)、
+ * 无符号整数 (u8, u16, u32, u64, usize) 、浮点数 (f32, f64)、以及有理数、复数
+ * 字符串：字符串字面量和字符串切片 &str
+ * 布尔类型： true和false
+ * 字符类型: 表示单个 Unicode 字符，存储为 4 个字节
+ * 单元类型: 即 () ，其唯一的值也是 ()
+ */
+fn variable_int_float() {
+    // // 编译器会进行自动推导，给予twenty i32的类型
+    // let twenty = 20;
+
+    // 通过类型后缀的方式进行类型标注：10是i32类型
+    let int_num = 10i32; //默认i32
+                         // 类型标注
+    let float_num: f64 = -1.23; //默认f64
+    println!("intNum is {}, floatNum is {}", int_num, float_num);
+
+    // NaN类型：数学上未定义的结果
+    let nan = float_num.sqrt(); // NaN
+    assert_eq!(nan, nan);
+
+    // 对于较长的数字，可以用_进行分割，提升可读性
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+
+    //运算: +, -, *, /, %
+
+    // 位运算：&位与, |位或, ^异或, !位非, <<左移, >>右移
+
+    // 序列Range 1..5, 1..=5
+    for i in 1..=5 {
+        println!("{}", i);
+    }
 }
