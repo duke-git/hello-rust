1b6b2317399138db48eff81fa755c93a401924ba
dudaodong <dudaodong@gmail.com>
2022-10-23 13:59:09 +0800

    add variable declare and destruct

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 fn main() {
-    // 变量绑定、可变性
-    // let mut x = 5;
-    // println!("x is {}", x);
-    // x = 6;
-    // println!("x is {}", x);
+    variable_declare();
+    variable_destruct();
 
-    // let _y = 10;
+    variable_shadow();
+}
 
-    //变量解构,解构赋值
-    // let (a, mut b) = (true, false);
-    // println!("a={:?}, b={:?}", a, b);
-    // b = true;
-    // assert_eq!(a, b);
+// 变量绑定、可变性
+fn variable_declare() {
+    let mut x = 5;
+    println!("x is {}", x);
+    x = 6;
+    println!("x is {}", x);
+
+    let _y = 10;
+}
+
+// 变量解构,解构赋值
+fn variable_destruct() {
+    let (a, mut b) = (true, false);
+    println!("a={:?}, b={:?}", a, b);
+    b = true;
+    assert_eq!(a, b);
 
     struct Struct {
         e: i32,
@@ -24,4 +33,22 @@ fn main() {
     Struct { e, .. } = Struct { e: 5 };
 
     assert_eq!([1, 2, 1, 4, 5], [a, b, c, d, e]);
+}
+
+/**
+ * 变量遮蔽:
+ * let生成了完全不同的新变量，两个变量只是恰好拥有同样的名称，涉及一次内存对象的再分配 ，
+ * 而mut声明的变量，可以修改同一个内存地址上的值，并不会发生内存对象的再分配，性能要更好
+ */
+fn variable_shadow() {
+    let x = 5;
+
+    let x = x + 1;
+    {
+        let x = x*2;
+        println!("innter x is: {}", x)
+    }
+
+    println!("outter x is: {}", x)
+
 }
